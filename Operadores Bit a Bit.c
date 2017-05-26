1f50493be08eea7e8fd33af353c5da344fee5fb6
kelvinneves <kelvinneves56@gmail.com>
2017-05-26 19:59:15 -0300

    Update Operadores Bit a Bit.c

--- Operadores Bit a Bit.c
+++ Operadores Bit a Bit.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <locale.h> 
+#include <locale.h>
 
 void op1(int, int);
 void op2(int, int);
@@ -17,65 +17,72 @@ banner();
 }
 
 void banner() {
-	int valor1 = 13;
-	int valor2 = 3;
-	printf("Encontro 13 - Javascript para C\n");
-	printf("Trabalhando com lógica bit a bit\n");
-	printf("\nValor A : %d", valor1);
-	printf("\nValor B : %d", valor2);
-	label:
-	printf("\n\nDigite :\n\n");
-	printf("1)OR\n2)AND\n3)XOR\n4)Deslocar para esquerda\n5)Deslocar para a direita\n\nEntrada: ");
-	scanf("%c", &entrada);
+int valor1 = 13;
+int valor2 = 3;
+printf("Encontro 13 - Javascript para C\n");
+printf("Trabalhando com lógica bit a bit\n");
+printf("\nValor A : %d", valor1);
+printf("\nValor B : %d", valor2);
+label:
+printf("\n\nDigite :\n\n");
+printf("1)OR\n2)AND\n3)XOR\n4)Deslocar para esquerda\n5)Deslocar para a direita\n\nEntrada: ");
+scanf("%c", &entrada);
+system("cls");
+	switch (entrada) {
+		case '1':
+	op1(valor1, valor2);
+	break;
+		case '2' :
+	op2(valor1, valor2);
+	break;
+		case '3' :
+	op3(valor1, valor2);
+	break;
+		case '4' :
+	op4(valor1, valor2);
+	break;
+		case '5' :
+	op5(valor1, valor2);
+	break;
+		default :
 	system("cls");
-		if (entrada == '1') {
-			op1(valor1, valor2);
-		} else if ( entrada == '2') {
-			op2(valor1, valor2);
-		} else if ( entrada == '3') {
-			op3(valor1, valor2);
-		} else if ( entrada == '4') {
-			op4(valor1, valor2);
-		} else if ( entrada == '5') {
-			op5(valor1, valor2);
-		} else {
-			system("cls");
-			printf("Entrada não encontrada\nTente novamente\n\n");
-			goto label;
-		}
+	printf("Entrada não encontrada\nTente novamente\n\n");
+	goto label;
+	break;
+	}
 }
 
 void op1(int a, int b) {
-itoa((a|b),saida,2);		//Converter o resultado para binário
-itoa(a, temp1, 2);		//Converter A para binário
-itoa(b, temp2, 2);		//Converter B para binário
+itoa((a|b),saida,2);	//Converter o resultado para binário
+itoa(a, temp1, 2);	//Converter A para binário
+itoa(b, temp2, 2);	//Converter B para binário
 printf("%d (%08s) OR %d (%08s) = %d (%08s)\n", a, temp1, b, temp2, (a|b), saida);
 }
 
 void op2(int a, int b) {
-itoa((a&b),saida,2);		//Converter o resultado para binário
-itoa(a, temp1, 2);		//Converter A para binário
-itoa(b, temp2, 2);		//Converter B para binário
+itoa((a&b),saida,2);	//Converter o resultado para binário
+itoa(a, temp1, 2);	//Converter A para binário
+itoa(b, temp2, 2);	//Converter B para binário
 printf("%d (%08s) AND %d (%08s) = %d (%08s)\n", a, temp1, b, temp2, (a&b), saida);
 }
 
 void op3(int a, int b) {
-itoa((a^b),saida,2);		//Converter o resultado
-itoa(a, temp1, 2);		//Converter A para binário
-itoa(b, temp2, 2);		//Converter B para binário
+itoa((a^b),saida,2);	//Converter o resultado
+itoa(a, temp1, 2);	//Converter A para binário
+itoa(b, temp2, 2);	//Converter B para binário
 printf("%d (%08s) XOR %d (%08s) = %d (%08s)\n", a, temp1, b, temp2, (a^b), saida);
 }
 
 void op4(int a, int b) {
-itoa((a<<b),saida,2);		//Converter o resultado para binário
-itoa(a, temp1, 2);		//Converter A para binário
-itoa(b, temp2, 2);		//Converter B para binário
+itoa((a<<b),saida,2);	//Converter o resultado para binário
+itoa(a, temp1, 2);	//Converter A para binário
+itoa(b, temp2, 2);	//Converter B para binário
 printf("%d (%08s) << %d (%08s) = %d (%08s)\n", a, temp1, b, temp2, (a<<b), saida);
 }
 
 void op5(int a, int b) {
-itoa((a>>b),saida,2);		//Converter o resultado para binário
-itoa(a, temp1, 2);		//Converter A para binário
-itoa(b, temp2, 2);		//Converter B para binário
+itoa((a>>b),saida,2);	//Converter o resultado para binário
+itoa(a, temp1, 2);	//Converter A para binário
+itoa(b, temp2, 2);	//Converter B para binário
 printf("%d (%08s) >> %d (%08s) = %d (%08s)\n", a, temp1, b, temp2, (a>>b), saida);
 }
