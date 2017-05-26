0f053776446a92ec6c2849d2e892edc820873fed
AlexandreVitorino <alexandre.almeida.n@gmail.com>
2017-05-26 14:06:29 -0300

    Update Operadores Bit a Bit.c
    removido 'char entrada', modificado a variável "c" para "entrada", pois foi declarada e esqueci de usar.

--- Operadores Bit a Bit.c
+++ Operadores Bit a Bit.c
@@ -8,10 +8,9 @@ void op2(int, int);
 void op3(int, int);
 void op4(int, int);
 void op5(int, int);
-
 void banner();
-int entrada;
-int saida;
+
+char entrada;
 
 int main() {
 setlocale(LC_ALL, "Portuguese");
@@ -28,16 +27,16 @@ void banner() {
 	label:
 	printf("\n\nDigite :\n\n");
 	printf("1)OR\n2)AND\n3)XOR\n4)Deslocar para esquerda\n5)Deslocar para a direita\n\nEntrada: ");
-	char c = cin.get();
-	if (c == '1') {
+	entrada = cin.get();
+	if (entrada == '1') {
 		op1(valor1, valor2);
-	} else if ( c == '2') {
+	} else if ( entrada == '2') {
 		op2(valor1, valor2);
-	} else if ( c == '3') {
+	} else if ( entrada == '3') {
 		op3(valor1, valor2);
-	} else if ( c == '4') {
+	} else if ( entrada == '4') {
 		op4(valor1, valor2);
-	} else if ( c == '5') {
+	} else if ( entrada == '5') {
 		op5(valor1, valor2);
 	} else {
 		system("cls");
